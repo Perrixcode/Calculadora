559b03f809a433423f2a8b51829a188c91cdd7c5
Perrixcode <estebaniturra@gmail.com>
2026-06-30 00:03:38 -0400

    Limpié el codigo

--- codigo.js
+++ codigo.js
@@ -1,4 +1,4 @@
-console.log("Calculator connected")
+/*console.log("Calculator connected")
 const display = document.querySelector("#display");
 
 const backspaceButton = document.querySelector(".backspace");
@@ -33,7 +33,7 @@ buttons.forEach((button) => {
         }
     }
     });
-});
+}); */
 function handleKeyboardInput(event) {
     console.log(event.key);
     if (event.key >= "0" && event.key <= "9"){
@@ -67,3 +67,47 @@ function handleKeyboardInput(event) {
 }
 
 document.addEventListener("keydown", handleKeyboardInput);
+
+console.log("Calculator connected")
+
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+function clearDisplay() {
+    display.value = "0";
+}
+function deleteLastCharacter() {
+    display.value = display.value.slice(0, -1);
+    if (display.value === ""){
+        display.value = "0";
+    }
+}
+function calculate() {
+    display.value = eval(display.value);
+}
+function appendToDisplay(value) {
+    if (display.value === "0"){
+        display.value = value;
+    } else {
+        display.value += value;
+    
+    }
+}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const buttonValue = button.textContent;
+        if (buttonValue === "AC"){
+            clearDisplay();
+        } else if (buttonValue === "⌫") {
+            deleteLastCharacter();
+        } else if (buttonValue === "="){
+            calculate();
+        } else if (buttonValue === ","){
+            appendToDisplay(".");
+
+        } else {
+            appendToDisplay(buttonValue);
+        }
+    });
+});
