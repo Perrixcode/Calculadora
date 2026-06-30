336ebac207a7fa5b8d95ca6af7d559559d3a0ce7
Perrixcode <estebaniturra@gmail.com>
2026-06-30 00:25:17 -0400

    Limpieza de codigo

--- codigo.js
+++ codigo.js
@@ -1,39 +1,4 @@
-/*console.log("Calculator connected")
-const display = document.querySelector("#display");
 
-const backspaceButton = document.querySelector(".backspace");
-
-
-
-const buttons = document.querySelectorAll("button");
-function deleteLastCharacter() {
-    display.value = display.value.slice(0, -1);
-    if (display.value === "") {
-        display.value = "0";
-    }
-}
-
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const buttonValue = button.textContent;
-
-        if (buttonValue === "AC") {
-            display.value = "0";
-        }else if (buttonValue === "⌫"){
-            deleteLastCharacter();
-        }else if (buttonValue === "="){
-            display.value = eval(display.value);
-        }else if (buttonValue === ","){
-            display.value += ".";
-        }else {
-            if (display.value === "0"){
-                display.value = buttonValue; 
-            }else {
-                display.value += buttonValue;
-        }
-    }
-    });
-}); */
 function handleKeyboardInput(event) {
     console.log(event.key);
     if (event.key >= "0" && event.key <= "9"){
